58b205990f0ffb7e7a0911ede127db08f07352ef
builtbydans <danish.shafi@live.co.uk>
2026-01-28 00:10:41 +0000

    vite setup, tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="min-h-screen bg-red-600 flex items-center justify-center">
+      <h1 className="text-5xl font-extrabold text-white">TAILWIND TEST</h1>
+    </div>
+  );
+};
+
+export default App;
